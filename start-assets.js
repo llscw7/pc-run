ca0847d809d4244b1a6a8179242bc90b058b590a
llscw7 <56208949+lqt0327@users.noreply.github.com>
2024-05-19 23:07:56 +0800

    chore: test

--- start-assets.js
+++ start-assets.js
@@ -4,8 +4,11 @@ const path = require('path')
 const fse = require('fs-extra')
 const { killPortProcess } = require('kill-port-process');
 const detect = require('detect-port');
+const bodyParser = require('body-parser')
 const app = express();
 
+fse.emptyDirSync('./assets_pc/cache')
+fse.ensureDirSync('./assets_pc/cache')
 fse.ensureDirSync('./assets_pc/images')
 
 const port = 38435
@@ -23,7 +26,13 @@ const port = 38435
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'assets_pc/images')
+    console.log(req.body, '9999', req.url, '---00', file)
+    if(req.url === '/upload') {
+      cb(null, 'assets_pc/cache')
+    }
+    // else if(req.url === '/submit') {
+    //   cb(null, 'assets_pc/images')
+    // }
   },
   filename: function (req, file, cb) {
     const ext = path.extname(file.originalname)
@@ -40,13 +49,27 @@ app.post('/upload', upload.single('image'), (req, res) => {
   // 处理上传的文件
   const file = req.file;
   // 进行文件处理或保存等操作
-  console.log(file,'???;;;898989')
   // 返回响应
   res.json({ status: {message: '文件上传成功', code: 0}, result: {url: `http://localhost:38435/${file.filename}`, oldName: file.originalname, name: file.filename}});
 });
 
+var jsonParser = bodyParser.json()
+
+app.post('/submit', jsonParser, (req, res) => {
+  console.log(req.body,'??>>>>>>---===')
+  const { data } = req.body
+  if(data && data.images && data.images.length) {
+    for(let img of data.images) {
+      fse.copyFileSync(`./assets_pc/cache/${img}`, `./assets_pc/images/${img}`)
+      console.log(`上传成功：${img}`)
+    }
+  }
+  res.json({ status: {message: 'success', code: 0}, result: data });
+})
+
 // 将指定目录下的文件作为静态资源提供给客户端
 app.use(express.static('assets_pc/images'));
+app.use(express.static('assets_pc/cache'));
 
 detect(port)
 .then(async (_port) => {
